82a060ce9f3eb7f96cfa239e88b33ff24a0f6d6a
Sebastian Nocke <sjno@gmx.de>
2024-01-01 19:32:58 +0100

    Basic start of CLI application

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,86 @@
+use std::process::exit;
+
+use inquire::Select;
 use simulator::{Circuit, Function, Value};
 
 fn main() {
-    let mut sim = Circuit::new();
-    let (_, value0) = sim.add_input(Value::On);
-    let (_, value1) = sim.add_input(Value::On);
+    let mut circuit = initialize();
+    let mut running = true;
 
-    let (_, values) = sim.add_component(Function::Or, vec![value0, value1]);
+    while running {
+        running = interaction(&mut circuit);
+    }
+}
 
-    let _ = sim.add_output(values[0]);
+fn initialize() -> Circuit {
+    let options = vec![
+        "Yes",
+        "No",
+    ];
+
+    let answer = Select::new("Do you want to create a new circuit simulation?", options).prompt();
+
+    match answer {
+        Ok(choice) => {
+            match choice {
+                "Yes" => {
+                    println!("Creating new empty circuit simulation!");
+                    Circuit::new()
+                },
+                "No" => {
+                    println!("Loading circuit simulations is currently not supported!");
+                    exit(0);
+                },
+                _ => simple_error_exiting(),
+            }
+        },
+        Err(_) => {
+            simple_error_exiting();
+        }
+    }
+}
+
+fn interaction(circuit: &mut Circuit) -> bool {
+
+    let options = vec![
+        "New input",
+        "New output",
+        "New element",
+        "Show all inputs",
+        "Show all outputs",
+        "Show all elements",
+        "Read Input",
+        "Read Output",
+        "Read Value",
+        "Exit",
+    ];
+
+    let answer = Select::new("What do you want to do?", options).prompt();
+
+    let choice = match answer {
+        Ok(choice) => choice,
+        Err(_) => simple_error_exiting(),
+    };
+
+    match choice {
+        "New input" => true,
+        "New output" => true,
+        "New element" => true,
+        "Show all inputs" => true,
+        "Show all outputs" => true,
+        "Show all elements" => true,
+        "Read Input" => true,
+        "Read Output" => true,
+        "Read Value" => true,
+        "Exit" => {
+            println!("Exiting...");
+            false
+        },
+        _ => simple_error_exiting(),
+    }
+}
+
+fn simple_error_exiting() -> ! {
+    println!("There was an error!");
+    exit(1)
 }
